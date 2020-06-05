d520b63adfaf8d3383c369971cfc5392135dd343
Tyler King <tyman.king97@gmail.com>
2020-06-05 15:35:29 -0700

    updated

---
+++ code_challenges/sort/challenges-03.test.js
@@ -0,0 +1,112 @@
+'use strict';
+
+// to learn more about the cheerio library and what it is doing, look at their documentation: https://www.npmjs.com/package/cheerio
+const cheerio = require('cheerio');
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 1 - Review
+
+Write a function named changeAllClassNames that uses jQuery to select all each li and add a class of "fruit";
+
+------------------------------------------------------------------------------------------------ */
+const $ = createSnippetWithJQuery(`
+<ul id="fruits">
+  <li class="apple">Apple</li>
+  <li class="orange">Orange</li>
+  <li>Pear</li>
+</ul>
+`);
+
+const changeAllClassNames = () => {
+  $('li').addClass('fruit')
+}
+
+
+describe('Testing challenge 1', () => {
+  test('It should add a class of fruit to all the list items', () => {
+    changeAllClassNames();
+
+    expect($('li.apple').hasClass('fruit')).toBe(true);
+    expect($('li.orange').hasClass('fruit')).toBe(true);
+  })
+})
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 2
+
+Write a function named sortBackwards that takes in an array of numbers and returns the same array, with the numbers sorted, highest to smallest.
+------------------------------------------------------------------------------------------------ */
+
+const sortBackwards = (arr) => arr.sort ((a,b)=> b-a);
+
+
+describe('Testing challenge 2', () => {
+  test('It should sort high-to-low the numbers in an array', () => {
+    const nums = [3,4,5,6,7];
+    expect(sortBackwards(nums)).toStrictEqual([7,6,5,4,3]);
+    expect(sortBackwards([3,2,1])).toStrictEqual([3,2,1]);
+    expect(sortBackwards([12,20,3])).toStrictEqual([20, 12, 3]);
+    expect(sortBackwards([])).toStrictEqual([]);
+    expect(sortBackwards([1])).toStrictEqual([1]);
+  });
+});
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 3
+
+Write a function named alphabetize that takes in an array of strings and returns the same array with the strings sorted alphabetically.
+
+In this alphabetization, capital letters come before lower case letters.
+
+For example, ['Alphabet', 'Zebra', 'alphabet', 'carrot'] is correctly sorted.
+------------------------------------------------------------------------------------------------ */
+
+const alphabetize = (arr) => {
+  arr.sort((a,b)=>{
+    if (a > b) {
+      return 1;
+    } else if (a < b){
+      return -1;
+    }
+  });
+  return arr;
+
+};
+
+describe('Testing challenge 3', () => {
+  test('It should sort strings alphabetically', () => {
+    expect(alphabetize(['alphabet', 'Zebra', 'Alphabet', 'carrot'])).toStrictEqual([ 'Alphabet', 'Zebra', 'alphabet', 'carrot']);
+    expect(alphabetize(['alphabet','Alphabet', 'carrot'])).toStrictEqual([ 'Alphabet', 'alphabet', 'carrot']);
+    expect(alphabetize([])).toStrictEqual([]);
+  });
+});
+
+
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 4
+
+Write a function named sortByLength that takes in an array of strings and returns the same array, with the strings sorted by their length, lowest to highest.
+------------------------------------------------------------------------------------------------ */
+
+const sortByLength = (arr) => arr.sort((a,b) => a.length - b.length);
+
+
+describe('Testing challenge 4', () => {
+  test('It should sort strings by length', () => {
+    const ans = sortByLength(['alphabet', 'Zebra', 'Alphabet', 'carrot']);
+    expect(ans.slice(0,2)).toStrictEqual(['Zebra', 'carrot']);
+    expect(ans.slice(2,4)).toEqual(expect.arrayContaining(['Alphabet', 'alphabet']));
+    expect(sortByLength(['a', 'bc', ''])).toStrictEqual(['', 'a', 'bc']);
+    expect(sortByLength(['a'])).toStrictEqual(['a']);
+    expect(sortByLength([])).toStrictEqual([]);
+  });
+});
+
+
+
+
+
+function createSnippetWithJQuery(html){
+  return cheerio.load(html);
+}
